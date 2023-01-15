ddbaa5d05c7dbaf24e0f3b91d2528ce2dad0c94b
craig <craig@oppkey.com>
2023-01-14 16:23:23 -0800

    added router

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,30 @@ import 'package:flame/parallax.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 
+import 'buttons.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Flame.device.setLandscapeLeftOnly();
   Flame.device.fullScreen();
-  runApp(GameWidget(game: ScrollGame()));
+  FlameGame game = ScrollGame();
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Stack(children: [
+          GameWidget(game: game),
+          Buttons(
+            game: game,
+          ),
+        ]),
+      ),
+    ),
+  );
 }
 
 class ScrollGame extends FlameGame with TapDetector {
+  late SpriteAnimationComponent crow;
+  Vector2 velocity = Vector2(0, 30);
   @override
   void onLoad() async {
     ParallaxComponent seaBackground = await loadParallaxComponent([
@@ -35,28 +51,29 @@ class ScrollGame extends FlameGame with TapDetector {
             stepTime: 0.1,
             textureSize: Vector2(350, 400)));
 
-    add(
-      SpriteAnimationComponent(
-          anchor: Anchor.center,
-          position: Vector2(size.x / 2, size.y * .6),
-          animation: crowAnimation,
-          size: Vector2(size.y * 350 / 400, size.y)
-          // sprite:
-          //     await loadSprite('crow_350x400.webp', srcSize: Vector2(350, 400)),
-          ),
-    );
-
+    crow = SpriteAnimationComponent(
+        anchor: Anchor.center,
+        position: Vector2(size.x / 2, size.y * .6),
+        animation: crowAnimation,
+        size: Vector2(size.y * 350 / 400, size.y)
+        // sprite:
+        //     await loadSprite('crow_350x400.webp', srcSize: Vector2(350, 400)),
+        );
+    add(crow);
     FlameAudio.bgm.initialize();
     FlameAudio.bgm.play('dramatic_boi.ogg');
   }
 
   @override
+  void update(double dt) {
+    super.update(dt);
+    velocity.y += .4;
+    crow.position += velocity * dt;
+  }
+
+  @override
   void onTap() {
-    if (FlameAudio.bgm.isPlaying) {
-      FlameAudio.bgm.pause();
-    } else {
-      FlameAudio.bgm.resume();
-    }
+    velocity.y -= 20;
     super.onTap();
   }
 }
